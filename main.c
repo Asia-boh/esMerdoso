39afc30ef7824da1de18b27db92c51ac1bc641d9
Asia-boh <163838315+Asia-boh@users.noreply.github.com>
2024-04-19 17:40:58 +0200

    madonna cane

--- main.c
+++ main.c
@@ -1,15 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
 struct Punto{
-    int x;
-    int y;
+    float x;
+    float y;
 };
-struct Centroide{
-    int x;
-    int y;
-    int posizione;
-};
+
+void azzeraVettoreFloat(float vettore[], int lunghezza){
+    for(int i = 0; i < lunghezza; i++){
+        vettore[i] = 0;
+    }
+}
+
+void azzeraVettoreInt(int vettore[], int lunghezza){
+    for(int i = 0; i < lunghezza; i++){
+        vettore[i] = 0;
+    }
+}
+
+float calcolaDistanza(float x1, float x2, float y1, float y2){
+    float xTot = (x1 - x2);
+    float yTot = (y1 - y2);
+    float d = sqrt(xTot*xTot + yTot*yTot);
+    return d;
+}
+
+void stampaPunti(struct Punto p[], int lunghezza){
+    for(int i = 0; i < lunghezza; i++){
+        printf("Punto %d: x = %f; y = %f\n", i+1, p[i].x, p[i].y);
+    }
+}
+
+int comparaCentroidi(struct Punto vecchiCentroidi[], struct Punto c[], int lunghezza){
+    int status = 0;
+    for(int i = 0; i < lunghezza; i++){
+        if(vecchiCentroidi[i].x == c[i].x && vecchiCentroidi[i].y == c[i].y){
+            status = 1;
+        }
+    }
+    return status;
+}
 
 int main()
 {
@@ -22,24 +54,67 @@ int main()
     struct Punto p[nPunti];
 
     for(int i = 0; i < nPunti; i++){
-        p[i].x = rand();
-        p[i].y = rand();
+        p[i].x = (float)rand() / RAND_MAX * 10000;
+        p[i].y = (float)rand() / RAND_MAX * 10000;
     }
+    stampaPunti(p, nPunti);
+    printf("\n\n");
 
     printf("Inserisci il numero di centroidi casuali da generare:\n");
     scanf("%d", &nCentroidi);
-    struct Centroide c[nCentroidi];
+    struct Punto c[nCentroidi];
 
     for(int i = 0; i < nCentroidi; i++){
-        int puntoRandom = rand()% nPunti;
+        int puntoRandom = rand()%nPunti;
         c[i].x = p[puntoRandom].x;
-        c[i].y = p[puntoRandom].x;
-        c[i].posizione = puntoRandom;
+        c[i].y = p[puntoRandom].y;
     }
-    int min = ;
-    for(int i = 0; i < nPunti; i++){
-        for(int j = 0; j < nCentroidi; j++){
+    stampaPunti(c, nCentroidi);
+    printf("\n\n");
 
+    struct Punto vecchioCentroide[nCentroidi];
+    for(int i = 0; i < nCentroidi; i++){
+        vecchioCentroide[i].x = c[i].x;
+        vecchioCentroide[i].y = c[i].y;
+    }
+    int counter = 0;
+    int equilibrio = 0;
+    while(equilibrio == 0){
+        int contatoreCentroidi[nCentroidi];
+        float mediaX[nCentroidi];
+        float mediaY[nCentroidi];
+        azzeraVettoreFloat(mediaX, nCentroidi);
+        azzeraVettoreFloat(mediaY, nCentroidi);
+        azzeraVettoreInt(contatoreCentroidi, nCentroidi);
+        for(int i = 0; i < nPunti; i++){
+            int pos = 0;
+            float min = calcolaDistanza(p[i].x, c[0].x, p[i].y, c[0].y);
+            for(int j = 1; j < nCentroidi; j++){
+                float d = calcolaDistanza(p[i].x, c[j].x, p[i].y, c[j].y);
+                if(min > d){
+                    min = d;
+                    pos = j;
+                }
+            }
+            mediaX[pos] += p[i].x;
+            mediaY[pos] += p[i].y;
+            contatoreCentroidi[pos]++;
+        }
+        for(int i = 0; i < nCentroidi; i++){
+            c[i].x = mediaX[i]/contatoreCentroidi[i];
+            c[i].y = mediaY[i]/contatoreCentroidi[i];
+        }
+        if(comparaCentroidi(vecchioCentroide, c, nCentroidi) == 0){
+            for(int i = 0; i < nCentroidi; i++){
+                vecchioCentroide[i].x = c[i].x;
+                vecchioCentroide[i].y = c[i].y;
+                counter++;
+            }
+        }
+        else{
+            equilibrio = 1;
+            stampaPunti(c, nCentroidi);
+            printf("\nSpostamenti: %d", counter);
         }
     }
 }
